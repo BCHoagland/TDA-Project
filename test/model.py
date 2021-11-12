46e5cc19879d5e766d6c7fd3ecdae13a5853e792
BCHoagland <bch29@duke.edu>
2021-11-11 21:50:38 -0500

    not sure what I did

--- test/model.py
+++ test/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import gudhi as gd
 
 
 class VAE(nn.Module):
@@ -66,3 +67,21 @@ class VAE(nn.Module):
     
     def maximize(self, loss):
         self._optimize(-loss)
+
+
+class RipsH1(nn.Module):
+    def __init__(self, max_edge_length):
+        super(RipsH1, self).__init__()
+        self.max_edge_length = max_edge_length
+
+    def forward(self, input):
+        rips = gd.RipsComplex(points=input, max_edge_length=self.max_edge_length)
+        st = rips.create_simplex_tree(max_dimension=2)
+        st.compute_persistence()
+        idx = st.flag_persistence_generators()
+        if len(idx[1]) == 0:
+            verts = torch.empty((0, 4), dtype=int)
+        else:
+            verts = torch.tensor(idx[1][0])
+        dgm = torch.norm(input[verts[:, (0, 2)]] - input[verts[:, (1, 3)]], dim=-1)
+        return dgm
--- test/train.py
+++ test/train.py
@@ -3,13 +3,14 @@ from torch.nn.functional import binary_cross_entropy
 from torchvision.utils import save_image
 from pathlib import Path
 import matplotlib.pyplot as plt
+import gudhi as gd
 
-from model import VAE
+from model import VAE, RipsH1
 from data import MNIST
 
 
 # hyperparameters
-num_epochs = 50
+num_epochs = 2
 batch_size = 128
 save_iter = 1
 
@@ -47,29 +48,118 @@ def plot_latent(val_data, model, epoch):
         plt.savefig(path + str(epoch) + '.png')
         # plt.show()
 
-plot_latent(val_data, model, 0)
+    # rips = gd.RipsComplex(
+    #     distance_matrix = encoded,
+    #     max_edge_length = 2
+    # ).create_simplex_tree(max_dimension=2)
+    # barcodes = rips.persistence()
+    # rips.compute_persistence()
+    # print(rips.betti_numbers())
+
+# plot_latent(val_data, model, 0)
+
+
+# ==========
+def rips_test():
+    rips = RipsH1(max_edge_length=0.5)
+
+    def h1_loss(pts):
+        dgm1 = rips(pts)
+        total_1pers = torch.sum(dgm1[:, 1] - dgm1[:, 0])
+        disk = (pts ** 2).sum(-1) - 1
+        disk = torch.max(disk, torch.zeros_like(disk)).sum()
+        return -total_1pers + 1*disk
+
+    with torch.no_grad():
+        orig_pts = model.encode(val_data[0])
+        # plt.clf()
+        # plt.scatter(orig_pts.numpy()[:, 0], orig_pts.numpy()[:, 1])
+        # plt.show()
+
+    from torch.optim.lr_scheduler import LambdaLR
+    opt = torch.optim.SGD(model.parameters(), lr=1e-4)
+    # scheduler = LambdaLR(opt, [lambda epoch: 10. / (10 + epoch)])
+    losses = []
+
+    for i in range(1000):
+        pts = model.encode(val_data[0])
+        opt.zero_grad()
+        l = h1_loss(pts)
+        l.backward()
+        losses.append(l.detach().item())
+        opt.step()
+        # scheduler.step()
+
+    plt.clf()
+    plt.plot(losses)
+    plt.show()
+    with torch.no_grad():
+        plt.clf()
+        plt.scatter(orig_pts.numpy()[:, 0], orig_pts.numpy()[:, 1], label='original')
+        # plt.show()
+
+        pts = model.encode(val_data[0])
+        # plt.clf()
+        plt.scatter(pts.numpy()[:, 0], pts.numpy()[:, 1], label='optimized')
+        plt.legend()
+        plt.show()
+
+# ripts_test()
+# ==========
 
 # start training
-for epoch in range(num_epochs):
+def train(topological):
+    rips = RipsH1(max_edge_length=0.5)
 
-    # minibatch optimization with Adam
-    for data in dataset.batches():
-        out, mean, log_var = model(data)
-        loss = binary_cross_entropy(out, data) + KL(mean, log_var)
-        model.minimize(loss)
+    def h1_loss(pts):
+        dgm1 = rips(pts)
+        total_1pers = torch.sum(dgm1[:, 1] - dgm1[:, 0])
+        disk = (pts ** 2).sum(-1) - 1
+        disk = torch.max(disk, torch.zeros_like(disk)).sum()
+        return -total_1pers + 1*disk
 
-    # save images periodically
-    if epoch % save_iter == save_iter - 1:
-        img = out.data.view(out.size(0), 1, 28, 28)
-        path = f'img/VAE/'
-        Path(path).mkdir(parents=True, exist_ok=True)
-        save_image(img, path + str(epoch + 1) + '_epochs.png')
+    model = VAE(data_size, n_h, n_latent)
 
-        plot_latent(val_data, model, epoch + 1)
+    epoch_losses = []
+    for epoch in range(num_epochs):
 
-    # report loss
-    print(f'Epoch {epoch}: loss {loss.item()}')
+        # minibatch optimization with Adam
+        batch_losses = []
+        for data in dataset.batches():
+            out, mean, log_var = model(data)
+            vae_loss = binary_cross_entropy(out, data) + KL(mean, log_var)
 
+            if topological:
+                topological_loss = h1_loss(out)     #! might have to fix later if the latent space is made more than 2D?
+            else:
+                topological_loss = 0
+
+            model.minimize(vae_loss + topological_loss)
+
+            with torch.no_grad():
+                batch_losses.append(vae_loss.item())
+
+        # save images periodically
+        if epoch % save_iter == save_iter - 1:
+            img = out.data.view(out.size(0), 1, 28, 28)
+            path = f'img/VAE/'
+            Path(path).mkdir(parents=True, exist_ok=True)
+            save_image(img, path + str(epoch + 1) + '_epochs.png')
+
+            plot_latent(val_data, model, epoch + 1)
+
+        # report loss
+        print(f'Epoch {epoch}: loss {vae_loss.item()}')
+        epoch_losses.append(sum(batch_losses) / len(batch_losses))
+
+    return epoch_losses
+
+l1 = train(topological=False)
+l2 = train(topological=True)
+plt.clf()
+plt.plot(l1, label='standard')
+plt.plot(l2, label='h1')
+plt.show()
 quit()
 
 # generate new random images
